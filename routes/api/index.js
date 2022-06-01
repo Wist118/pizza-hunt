b18417bee25028981cab4ff17510e727071a58ae
William Stock <grim118@hotmail.com>
2022-06-01 16:41:17 -0500

    added comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+// prefix of `/comments` from `comment-routes.js`
+router.use('/comments', commentRoutes);
 
 module.exports = router;
